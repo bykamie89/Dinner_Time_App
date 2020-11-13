2ce4826ef655fecdc2fa745815f8891d07481bf2
kamilla nødvig <kamilla@MacBook-Pro-tilhrende-kamilla.local>
2020-11-13 14:37:38 +0100

    bottom tab 1

--- App.js
+++ App.js
@@ -1,13 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import {
+  faHome,
+  faUser,
+  faHeart,
+  faShoppingBasket,
+  faBookOpen,
+} from '@fortawesome/free-solid-svg-icons';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import UserScreen from './app/screens/UserScreen';
+import FavoritesScreen from './app/screens/FavoritesScreen';
+import ShoppingCartScreen from './app/screens/ShoppingCartScreen';
+import RecipeScreen from './app/screens/RecipeScreen';
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        activeTintColor: '#e91e63',
+      }}
+    >
+      <Tab.Screen
+        name='WelcomeScreen'
+        component={WelcomeScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => <FontAwesomeIcon icon={faHome} />,
+        }}
+      />
+      <Tab.Screen
+        name='UserScreen'
+        component={UserScreen}
+        options={{
+          tabBarLabel: 'User',
+          tabBarIcon: ({ color, size }) => <FontAwesomeIcon icon={faUser} />,
+        }}
+      />
+      <Tab.Screen
+        name='FavoritesScreen'
+        component={FavoritesScreen}
+        options={{
+          tabBarLabel: 'Favorites',
+          tabBarIcon: ({ color, size }) => <FontAwesomeIcon icon={faHeart} />,
+        }}
+      />
+      <Tab.Screen
+        name='ShoppingCartScreen'
+        component={ShoppingCartScreen}
+        options={{
+          tabBarLabel: 'Shopping cart',
+          tabBarIcon: ({ color, size }) => <FontAwesomeIcon icon={faShoppingBasket} />,
+        }}
+      />
+      <Tab.Screen
+        name='RecipeScreen'
+        component={RecipeScreen}
+        options={{
+          tabBarLabel: 'Recipies',
+          tabBarIcon: ({ color, size }) => <FontAwesomeIcon icon={faBookOpen} />,
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
   );
 }
 
---
+++ app/screens/UserScreen.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+
+function UserScreen(props) {
+  return (
+    <View style={styles.background}>
+      <View style={styles.logoContainer}>
+              <Text style={styles.logoText}>User</Text>
+      </View>
+      <View></View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: 'white',
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: 'absolute',
+    top: 70,
+    alignItems: 'center',
+  },
+  logoText: {
+    fontStyle: 'normal',
+    fontSize: 30,
+  },
+  navigationBottomBar: {
+    flex: 1,
+    width: '100%',
+    height: 70,
+    backgroundColor: 'lightgrey',
+  },
+});
+
+export default UserScreen;
